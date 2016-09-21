cb7093b6255132d7b08f200ca963da398f881440
yujianzhen <787908531@qq.com>
2016-09-21 14:55:07 +0800

    更新界面

---
+++ yh-home/src/main/web/product/js/danpin/main.js
@@ -0,0 +1,291 @@
+$(document).ready(function(){
+    jQuery.support.cors = true;//跨域
+    $(function() {
+        $(".buttom_w").click(function () {
+            $(".province").toggle();
+        })
+        $(".sel_data em").click(function(){
+            $(this).next("dl").toggle();
+        })
+        $(".sel_data dd").click(function(){
+            $(this).parent("dl").siblings("em").text($(this).text());
+            $(this).parent("dl").hide();
+        })
+    })
+
+    $(function(){
+        $(".fomr_type li").last().css({"border-right":"none"})
+    })
+
+    /*****省份*********/
+    $(function(){
+        $(".province dd em").click(function(){
+            $(".buttom_w").text($(this).text());
+            $(".province").hide();
+        })
+    })
+
+    /*******form********/
+    $(function(){
+        var pageTotal="";
+        var _pageSize = 3;	//每页数量
+        var _isPageInit = false;    //是否已初始化分页控件
+        var formDiv = $(".seek_list input");
+        var selectDiv = $(".selectClass");
+
+        function add(page){
+            var param ={
+                breedName:formDiv.eq(0).val(),
+                miDiameterMin:new Number(formDiv.eq(1).val()),
+                miDiameterMax:new Number(formDiv.eq(2).val()),
+                updateTimeMin:formDiv.eq(3).val(),
+                updateTimeMax:formDiv.eq(4).val(),
+                heightMin:new Number(formDiv.eq(5).val()),
+                heightMax:new Number(formDiv.eq(6).val()),
+                crownMin:new Number(formDiv.eq(7).val()),
+                crownMax:new Number(formDiv.eq(8).val()),
+                area:"1001",
+                source:selectDiv.eq(0).val(),
+                invoiceType:selectDiv.eq(1).val(),
+                skip:page,
+                limit:_pageSize,
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: "http://192.168.200.45:9090/price/findProduct",
+                data: {"param" : JSON.stringify(param)},
+                dataType: "json",
+                /*contentType:""*/
+                success: function(data){
+                    var count=data.count;
+                    var topDiv = $(".fomr_top em");
+                    pageTotal=count[0].countTotal;
+                    topDiv.eq(0).text(count[0].countTotal);
+                    topDiv.eq(1).text(count[1].supplierTotal);
+                    topDiv.eq(2).text(count[0].miDiameterMin);
+                    topDiv.eq(3).text(count[0].miDiameterMax);
+                    topDiv.eq(4).text(count[0].FareMin);
+                    topDiv.eq(5).text(count[0].FareMax);
+                    topDiv.eq(6).text(count[0].FareAvg);
+                    topDiv.eq(7).text(count[0].TotalPriceMin);
+                    topDiv.eq(8).text(count[0].TotalPriceMax);
+                    topDiv.eq(9).text(count[0].PriceAvg);
+                    var trhtml="";
+                    trhtml+="<tr>"
+                        +"<td class='location'>"
+                        +"产品名称"
+                        +"<span class='sort'>"
+                            +"<img src='images/paixun.png'>"
+                        +"</span>"
+                        +"</td>"
+                        +"<td>"
+                        +"胸径/米径"
+                        +"</td>"
+                        +"<td>"
+                        +"高度"
+                        +"</td>"
+                        +"<td>"
+                        +"冠幅"
+                        +"</td>"
+                        +"<td>"
+                        +"上车价"
+                        +"</td>"
+                        +"<td>"
+                        +"发票类型"
+                        +"</td>"
+                        +"<td>"
+                        +"到货价"
+                        +"</td>"
+                        +"<td>"
+                        +"地区"
+                        +"</td>"
+                        +"<td>"
+                        +"供应商"
+                        +"</td>"
+                        +"<td>"
+                        +"联系人"
+                        +"</td>"
+                        +"<td>"
+                        +"手机"
+                        +"</td>"
+                        +"<td>"
+                        +"数据源"
+                        +"</td>"
+                        +"<td>"
+                        +"更新时间"
+                        +"</td>"
+                        +"<td>"
+                        +"操作"
+                        +"</td>"
+                        +"</tr>";
+
+                    var liHTML = "";
+                    var data=data.result;
+
+                    var _priceAl = "";//税率
+                    var _price = " ";//税率
+                    _price = $("#taxRate").val();
+                    var _newstr=_price.replace(/%/, "");
+                    _decimal = _newstr/100;
+                    var tax =(_decimal+1);//税后
+
+                    for (var i= 0,max= data.length;i < max; i++ ) {
+                        priceAll= data[i].startingFare*tax;
+                        var decimal = priceAll.toFixed(2)
+                        liHTML += "<tr>"
+                            + "<td>"
+                            + data[i].productName
+                            + "</td>"
+                            + "<td>"
+                            + data[i].miDiameter
+                            + "</td>"
+                            + "<td>"
+                            + data[i].height
+                            + "</td>"
+                            + "<td>"
+                            + data[i].crown
+                            + "</td>"
+                            + "<td name='mm'>"
+                            + /*data[i].startingFare*/decimal
+                            + "</td>"
+                            + "<td>"
+                            + data[i].invoiceType
+                            + "</td>"
+                            + "<td>"
+                            + data[i].totalPrice
+                            + "</td>"
+                            + "<td>"
+                            + data[i].area
+                            + "</td>"
+                            + "</td>"
+                            + "<td>"
+                            + data[i].supplier
+                            + "</td>"
+                            + "<td>"
+                            + data[i].contacts
+                            + "</td>"
+                            + "<td>"
+                            + data[i].tel
+                            + "</td>"
+                            + "<td>"
+                            + data[i].source
+                            + "</td>"
+                            + "<td>"
+                            + data[i].updateTime
+                            + "</td>"
+                            + "<td>"
+                            + "<a href='#' class='badge bg-red'>"
+                            + "查询运费"
+                            + "</a>"
+                            + "<a href='#' class='badge bg-red'>"
+                            + "详情"
+                            + "</a>"
+                            + "</td>"
+                            + "</tr>";
+                    }
+                    $(".table-bordered").html(trhtml + liHTML);
+
+                    /*****分页初始化*****/
+                    if(!_isPageInit){
+                        $("#paging").pagination(pageTotal, {
+                            items_per_page: _pageSize,	//每页数量
+                            num_display_entries: 5,
+                            num_edge_entries: 2,
+                            prev_text: "上一页",
+                            next_text: "下一页",
+                            callback:function(page){
+                                /*console.log("" + page);*/
+                                add(page);
+                            }
+                        });
+                        _isPageInit = true;
+                    }
+
+                    /****税率***/
+                    function rate(data) {
+                        var _priceAl = "";//税率
+                        var _price = " ";//税率
+                        _price = $("#taxRate").val();
+                        var _newstr=_price.replace(/%/, "");
+                        _decimal = _newstr/100;
+                        var mm =(_decimal+1);//税后
+                        for(var i= 0,max= data.length;i < max; i++){
+                           /* var bb=data[i].startingFar;*/
+                            priceAll= data[i].startingFare*mm;
+                           /* var nn = Math.floor(priceAll*100)/100;*/
+                            $("[name='mm']").eq(i).html(priceAll.toFixed(2));
+                        }
+                       /* alert(bb);*/
+                    }
+                    $("#taxRate").change(function(){
+                        rate(data);
+                    })
+
+
+
+                },
+                error:function(data,msg){
+                    alert(msg);
+                }
+            });
+
+        }
+
+        //查询
+        $("#search").on("click",function(){
+            add(1);
+            /*rate();*/
+            $(".fomr_data li").last().css({"border-right":"none"})
+            $(".main_form").show();
+        })
+    })
+
+
+    /********excel**********/
+    $(function(){
+        /***获得文件路径***/
+        $(".a-upload").on("change","input[type='file']",function(){
+            var filePath=$(this).val();
+            console.log(filePath);
+            if(filePath.indexOf("xls")!=-1 || filePath.indexOf("xlsx")!=-1){
+                /*$(".fileerrorTip").html("").hide();*/
+                var arr=filePath.split('\\');
+                var fileName=arr[arr.length-1];
+                $(".showFileName").html(fileName);
+                /*$('#button').removeAttr("disabled",'true');*/
+            }else{
+                $(".showFileName").html("上传文件类型有误！");
+                /*return false;*/
+                /*$("#uploading").attr('disabled','true');*/
+                return false;
+            }
+        })
+
+        /******上传文件*****/
+       /* $("#uploading").click(function(){
+            var filename = $("#file").val();
+            console.log(filename)
+            $.ajax({
+                url: "http://192.168.200.45:9090/price/excelData",
+                type: "POST",
+                dataType: "json",
+                enctype:"multipart/form-data",
+                data: {
+                    file: filename
+                },
+                success: function (data) {
+                    console.log(cosole);
+                },
+                error:function(){
+                    alert(11)
+                }
+            });
+        })*/
+
+        $("#excel").on("click",function(){
+            $(".folder").show();
+        })
+    })
+
+})
---
+++ yh-home/src/main/web/product/js/freight/freight.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+    // 百度地图API功能
+    $(function(){
+        var map = new BMap.Map("allmap");
+        var start = "";//始点
+        var end = "";//终点
+        var output = "";//距离
+        if(localStorage.getItem("start") !=null) $("#cityBegin").val(localStorage.getItem("start"));
+        map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
+        //三种驾车策略：最少时间，最短距离，避开高速
+       /* var routePolicy = [BMAP_DRIVING_POLICY_LEAST_TIME,BMAP_DRIVING_POLICY_LEAST_DISTANCE,BMAP_DRIVING_POLICY_AVOID_HIGHWAYS];*/
+        function baidu(){
+            $(".distanceCost").show();
+            $(".allmap").show();
+            //路线
+            start = $("#cityBegin").val()/*"天安门"*/;
+            end = $("#cityFinish").val()/*"百度大厦"*/;
+            localStorage.setItem("start",start);//保存本地地址
+            map.clearOverlays();
+            /* var i=$("#driving_way select").val();*/
+            search(start,end/*,routePolicy*//*[i]*/);
+            function search(start,end,route){
+                var driving = new BMap.DrivingRoute(map, {renderOptions:{map: map, autoViewport: true},policy: route});
+                driving.search(start,end);
+            }
+
+            //总的距离
+            var searchComplete = function (results){
+                if (transit.getStatus() != BMAP_STATUS_SUCCESS){
+                    return ;
+                }
+                var plan = results.getPlan(0);//获得路线
+                output = plan.getDistance(true) //获取距离
+                var Nun = "";
+                Nun = parseFloat(output);
+                //计算费用
+                var freightMin = 10;//5.2米货车每公里的费用最少
+                var freightMax = 22; //5.2米货车每公里费用最多
+                var freightSecondMin = 30;//9.6米货车每公里的费用
+                var freightSecondMax = 40; //9.6米货车每公里费用
+                var freightThirdlyMin = 50;//13.5米货车每公里的费用
+                var freightThirdlyMax = 60; //13.5米货车每公里费用
+                var freightFourthlyMin = 70;//17.5米货车每公里的费用
+                var freightFourthlyMax = 80; //17.5米货车每公里费用
+                var fMin = "";//5.2
+                var fMax = "";//5.2
+                var fSMin = "";//9.6
+                var fSMax = "";//9.6
+                var fTMin = "";//13.5
+                var fTMax = "";//13.5
+                var fFMin = "";//17.5
+                var fFMax = "";//17.5
+                fMin= freightMin*Nun;//5.2
+                fMax = freightMax*Nun;//5.2
+                fSMin = freightSecondMin*Nun;//9.6
+                fSMax = freightSecondMax*Nun;//9.6
+                fTMin = freightThirdlyMin*Nun;//13.5
+                fTMax = freightThirdlyMax*Nun;//13.5
+                fFMin = freightFourthlyMin*Nun;//17.5
+                fFMax = freightFourthlyMax*Nun;//17.5
+                $(".distance em").eq(0).text(fMin.toFixed(2));
+                $(".distance em").eq(1).text(fMax.toFixed(2));
+                $(".distance em").eq(2).text(fSMin.toFixed(2));
+                $(".distance em").eq(3).text(fSMax.toFixed(2));
+                $(".distance em").eq(4).text(fTMin.toFixed(2));
+                $(".distance em").eq(5).text(fTMax.toFixed(2));
+                $(".distance em").eq(6).text(fFMin.toFixed(2));
+                $(".distance em").eq(7).text(fFMax.toFixed(2));
+
+            }
+            //赋值路程
+            var transit = new BMap.DrivingRoute(map, {renderOptions: {map: map},
+                onSearchComplete: searchComplete,
+                onPolylinesSet: function(){
+                    $("#allDistance").text(output)
+                }
+            });
+            transit.search(start, end);
+        }
+        $("#cityInquire").click(function(){
+            baidu();
+        });
+        /*$(".cityInquire").keydown(function(){
+            baidu();
+        })*/
+        $(this).keydown(function (e){
+            var key = e.which;
+            console.log(key);
+            if(key == "13"){
+                baidu();
+            }
+        })
+        /*$('.cityInquire:first').focus();
+        var $inp = $('.cityInquire');
+        $inp.bind('keydown', function (e) {
+            var key = e.which;
+            if (key == 13) {
+                e.preventDefault();
+                var nxtIdx = $inp.index(this) + 1;
+                $(":input:text:eq(" + nxtIdx + ")").focus();
+            }
+            alert(key)
+        });*/
+    })
+})
